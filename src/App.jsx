de4e0b074433d9e4507f7e27a9a63505c5a54c45
ERI <sakaenet@gmail.com>
2026-04-15 08:29:44 +0900

    曲データを外部ファイルに分離

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,9 @@
 import { useState } from "react";
+import "./App.css";
 import BpmFilter from "./BpmFilter";
 import GenreFilter from "./GenreFilter";
 import SongList from "./SongList";
-import "./App.css";
-
-const songs = [
-  { id: 1, title: "Shape of You", artist: "Ed Sheeran", bpm: 96, genre: "Pop" },
-  {
-    id: 2,
-    title: "Blinding Lights",
-    artist: "the Weeknd",
-    bpm: 171,
-    genre: "Pop",
-  },
-  { id: 3, title: "Levitating", artist: "Dua Lipa", bpm: 103, genre: "Pop" },
-  { id: 4, title: "Bad Guy", artist: "Billie Eilish", bpm: 135, genre: "Pop" },
-  {
-    id: 5,
-    title: "Uptown Funk",
-    artist: "Bruno Mars",
-    bpm: 115,
-    genre: "Funk",
-  },
-  {
-    id: 6,
-    title: "Lose Yourself",
-    artist: "Eminem",
-    bpm: 171,
-    genre: "HipHop",
-  },
-  {
-    id: 7,
-    title: "HUMBLE",
-    artist: "Kendrick Lamer",
-    bpm: 150,
-    genre: "HipHop",
-  },
-  {
-    id: 8,
-    title: "Titanium",
-    artist: "Davie Guetta",
-    bpm: 126,
-    genre: "EDM",
-  },
-  {
-    id: 9,
-    title: "Clarity",
-    artist: "Zedd",
-    bpm: 128,
-    genre: "EDM",
-  },
-  {
-    id: 10,
-    title: "Wake Me Up",
-    artist: "Avicii",
-    bpm: 124,
-    genre: "EDM",
-  },
-];
+import songs from "./data/songs";
 
 const genres = ["All", ...new Set(songs.map((song) => song.genre))];
 
