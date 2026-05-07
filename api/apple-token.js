c692c79ea547b25f3fb7353f19d360d6588887d9
ERI <sakaenet@gmail.com>
2026-05-08 08:15:57 +0900

    Apple Music APIトークン import形式に修正

--- api/apple-token.js
+++ api/apple-token.js
@@ -1,19 +1,34 @@
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-module.exports = function handler(req, res) {
-  const privateKey = process.env.APPLE_PRIVATE_KEY.replace(/\\n/g, "\n");
-  const teamId = process.env.APPLE_TEAM_ID;
-  const keyId = process.env.APPLE_KEY_ID;
+export default function handler(req, res) {
+  try {
+    const privateKeyRaw = process.env.APPLE_PRIVATE_KEY;
+    const teamId = process.env.APPLE_TEAM_ID;
+    const keyId = process.env.APPLE_KEY_ID;
 
-  const token = jwt.sign({}, privateKey, {
-    algorithm: "ES256",
-    expiresIn: "180d",
-    issuer: teamId,
-    header: {
-      alg: "ES256",
-      kid: keyId,
-    },
-  });
+    if (!privateKeyRaw || !teamId || !keyId) {
+      return res.status(500).json({
+        error: "Missing env vars",
+        hasKey: !!privateKeyRaw,
+        hasTeam: !!teamId,
+        hasKeyId: !!keyId,
+      });
+    }
 
-  res.status(200).json({ token });
-};
+    const privateKey = privateKeyRaw.replace(/\\n/g, "\n");
+
+    const token = jwt.sign({}, privateKey, {
+      algorithm: "ES256",
+      expiresIn: "180d",
+      issuer: teamId,
+      header: {
+        alg: "ES256",
+        kid: keyId,
+      },
+    });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
